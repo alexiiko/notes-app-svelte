306baf9b1550223d64cc09592cd2840a6fdbfcab
alexiiko <alexiiko.msl@gmail.com>
2023-11-28 19:07:15 +0100

    added note and buttons for deleting and adding notes

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import Note from "./note.svelte";
+
+  let notesList: any = []
+
+  function addNote()
+  {
+    let key: number = notesList.length + 1
+    notesList = [...notesList, { key }]
+  }
+
+  function deleteLastNote()
+  {
+    notesList.pop()
+  }
+</script>
+
+
+<main>
+  <div>
+    <button on:click={addNote}>Add note</button>
+    <button on:click={deleteLastNote}>Delete last note</button>
+  </div>
+
+  {#each notesList as {key}}
+    <div>
+      <Note />
+    </div>
+  {/each}       
+</main>
+
---
+++ src/note.svelte
@@ -0,0 +1,11 @@
+<main>
+    <textarea cols="30" rows="10"></textarea>
+</main>
+
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap");
+
+    textarea {
+        font-family: "Open Sans", sans-serif;
+    }
+</style>
